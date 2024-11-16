370716d327bd67194cf1c5aa90dd9c8bfd3de0b4
kdiffin <vhiunese@gmail.com>
2024-11-16 19:38:00 +0400

    vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,9 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +17,7 @@ export default defineConfig({
       theme: "nord",
       wrap: false
     }
-  }
+  },
+  output: "server",
+  adapter: vercel()
 });
