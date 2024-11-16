1727195f1fe02d10d56364d4764095c0f4716523
kdiffin <vhiunese@gmail.com>
2024-11-16 19:44:15 +0400

    err fixed

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,26 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_B95MzbtD.mjs';
+import { manifest } from './manifest_DUr4jU1A.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "237d2ecd-5a96-42dd-8b78-80d26810171b",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
