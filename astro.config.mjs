61c7c3bbf30b27112700c1f2058559fa68f80514
kdiffin <vhiunese@gmail.com>
2024-11-16 19:49:33 +0400

    potential fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-import { SITE_URL } from "./src/data/config";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
@@ -10,14 +9,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt(), react()],
-  site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false
-    }
-  },
+
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
